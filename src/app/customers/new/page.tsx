d2e1c6cef6834c3ce00a0c21d6d8cc8506c6c87e
qians950-web <qians950@gmail.com>
2026-07-09 00:27:44 +0900

    Add navigation link to customers list on new page

--- src/app/customers/new/page.tsx
+++ src/app/customers/new/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import CustomerForm from "../CustomerForm";
 import { createCustomer } from "../actions";
 import { requireUser } from "@/lib/dal";
@@ -8,6 +9,9 @@ export default async function NewCustomerPage() {
     <div className="flex flex-col gap-6">
       <h1 className="text-xl font-semibold">顧客の新規登録</h1>
       <CustomerForm action={createCustomer} submitLabel="登録する" />
+      <Link href="/customers" className="text-sm text-zinc-500 hover:underline">
+        ← 一覧に戻る
+      </Link>
     </div>
   );
 }
